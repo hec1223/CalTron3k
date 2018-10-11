82cf61493669f784900a07f1e6d4fb184e610606
Rollins <krollins@itron.com>
2018-10-11 11:20:14 -0500

    add initial codebase

--- CalTron3k/MainWindow.xaml.cs
+++ CalTron3k/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net.Mail;
 
 namespace CalTron3k
 {
@@ -23,6 +24,28 @@ namespace CalTron3k
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public string Mailer(string username, string password)
+        {
+           string status = "Message has been sent";
+            
+           MailMessage message = new MailMessage();
+           SmtpClient SmtpServer = new SmtpClient("smtp-mail.outlook.com");
+           
+           SmtpServer.EnableSsl = true;
+           message.From = new MailAddress(username);
+           message.To.Add(username);
+           message.Subject = "TEST Subject";
+           message.Body = "Body Test";
+
+           SmtpServer.Port = 587;
+           
+           SmtpServer.Credentials = new System.Net.NetworkCredential(username, password);
+           
+           SmtpServer.Send(message);
+
+           return status; 
         }
     }
 }
